22f216cdde896dddc40cbe291c73a9e49c9f4f1d
DLegoT <lybomyrdakh@gmail.com>
2018-02-22 12:08:11 +0200

    Form send test #2

--- testfront/project038/send.php
+++ testfront/project038/send.php
@@ -1,19 +1,17 @@
-<?
-if((isset($_POST['name'])&&$_POST['name']!="")&&(isset($_POST['phone'])&&$_POST['phone']!="")){
-        $to = 'lybomyrdakh@gmail.com'; 
-        $subject = 'Обратный звонок'; 
-        $message = '
-                <html>
-                    <head>
-                        <title>'.$subject.'</title>
-                    </head>
-                    <body>
-                        <p>Имя: '.$_POST['name'].'</p>
-                        <p>Телефон: '.$_POST['phone'].'</p>                        
-                    </body>
-                </html>'; 
-        $headers  = "Content-type: text/html; charset=utf-8 \r\n"; 
-        $headers .= "From: Отправитель <from@example.com>\r\n"; 
-        mail($to, $subject, $message, $headers);
-}
-?>
+<?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено"; 
+} else { 
+    echo "при отправке сообщения возникли ошибки";
+}?>
